95ef0971d7027ebb00768839e05a8267c28e2049
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-15 23:48:48 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self #initialize is technically an instance method and self is the instance it creates here
+  end
+ 
+  def self.all
+    return @@all
+  end
+
+end
